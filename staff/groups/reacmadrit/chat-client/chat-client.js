0ede9c5b89cb98cd9e81f1bce6f62908ac6b9b35
Melinx <indraroura@gmail.com>
2018-05-17 11:23:14 +0200

    x

--- staff/groups/reacmadrit/chat-client/chat-client.js
+++ staff/groups/reacmadrit/chat-client/chat-client.js
@@ -5,9 +5,11 @@ const [url, port, from, message] = process.argv.slice(2)
 
 http.get(`http://${url}:${port}/?from=${from}&message=${message}`, (res) => {
     res.setEncoding('utf8')
+    res.on('error', console.error)
     res.on('data',chunk => data+=chunk)
     res.on('end', () => {
         console.log(data)
     })
 
 }).on('error', console.error)
+
