0d79615efcd3356c09cdeab05e3fd232cf0076af
Melinx <indraroura@gmail.com>
2018-06-06 11:57:27 +0200

    d

--- staff/Melinx/myApp/server/data/courses/courses.js
+++ staff/Melinx/myApp/server/data/courses/courses.js
@@ -25,6 +25,7 @@ let dish = Course.create({
     baseFood: 'rice',
     daysAvail: 'Thurssday',
 })
+
 let dish = Course.create({
     category: 'secondCourse',
     image: 'https://res.cloudinary.com/elsgerds/image/upload/v1528204567/2-17_salmon-teryaki.jpg',
@@ -33,6 +34,333 @@ let dish = Course.create({
     baseFood: 'fish',
     daysAvail: 'Wednesday',
 })
+
+let dish = Course.create({
+    category: 'secondCourse',
+    image: 'https://res.cloudinary.com/elsgerds/image/upload/v1528204567/2-10_gall-peix.jpg',
+    dishName: 'Chicken',
+    temp: 'hot',
+    baseFood: 'meat',
+    daysAvail: 'Friday',
+})
+
+let dish = Course.create({
+    category: 'secondCourse',
+    image: 'https://res.cloudinary.com/elsgerds/image/upload/v1528204567/2-18_tomato-pasta.jpg',
+    dishName: 'Pasta tomato',
+    temp: 'hot',
+    baseFood: 'pasta',
+    daysAvail: 'Tuesday',
+})
+
+let dish = Course.create({
+    category: 'secondCourse',
+    image: 'https://res.cloudinary.com/elsgerds/image/upload/v1528204567/2-16_romesco-de-conill.jpg',
+    dishName: 'Rabbit romesco',
+    temp: 'hot',
+    baseFood: 'meat',
+    daysAvail: 'Thursday',
+})
+
+let dish = Course.create({
+    category: 'secondCourse',
+    image: 'https://res.cloudinary.com/elsgerds/image/upload/v1528204567/2-15_potaje.jpg',
+    dishName: 'Garbanzos & meatballs ',
+    temp: 'hot',
+    baseFood: 'bean',
+    daysAvail: 'Tuesday',
+})
+let dish = Course.create({
+    category: 'secondCourse',
+    image: 'https://res.cloudinary.com/elsgerds/image/upload/v1528204567/2-14_peus-de-porc.jpg',
+    dishName: 'Pork feet',
+    temp: 'hot',
+    baseFood: 'meat',
+    daysAvail: 'Tuesday',
+})
+let dish = Course.create({
+    category: 'secondCourse',
+    image: 'https://res.cloudinary.com/elsgerds/image/upload/v1528204567/2-13_pasta-truffle-carbonara.jpg',
+    dishName: 'Truffle carbonara',
+    temp: 'hot',
+    baseFood: 'pasta',
+    daysAvail: 'Thursday',
+})
+let dish = Course.create({
+    category: 'secondCourse',
+    image: 'https://res.cloudinary.com/elsgerds/image/upload/v1528204567/2-11_mandonguilles.jpg',
+    dishName: 'Meatballs & peas',
+    temp: 'hot',
+    baseFood: 'meat',
+    daysAvail: 'Monday',
+})
+let dish = Course.create({
+    category: 'secondCourse',
+    image: 'https://res.cloudinary.com/elsgerds/image/upload/v1528204566/2-9_fricando.jpg',
+    dishName: 'Fricandó',
+    temp: 'hot',
+    baseFood: 'meat',
+    daysAvail: 'Thursday',
+})
+let dish = Course.create({
+    category: 'secondCourse',
+    image: 'https://res.cloudinary.com/elsgerds/image/upload/v1528204566/2-8_fideua.jpg',
+    dishName: 'Fideuà',
+    temp: 'hot',
+    baseFood: 'pasta',
+    daysAvail: 'Wednesday',
+})
+
+let dish = Course.create({
+    category: 'secondCourse',
+    image: 'https://res.cloudinary.com/elsgerds/image/upload/v1528204566/2-6_cargols-a-la-launa.jpg',
+    dishName: '"Canned" snails',
+    temp: 'hot',
+    baseFood: 'pasta',
+    daysAvail: 'Friday',
+})
+let dish = Course.create({
+    category: 'secondCourse',
+    image: 'https://res.cloudinary.com/elsgerds/image/upload/v1528204566/2-4_black-rice.jpg',
+    dishName: 'Black rice',
+    temp: 'hot',
+    baseFood: 'rice',
+    daysAvail: 'Monday',
+})
+let dish = Course.create({
+    category: 'secondCourse',
+    image: 'https://res.cloudinary.com/elsgerds/image/upload/v1528204566/2-7_conil-amb-cargols-allioli.jpg',
+    dishName: 'Rabbit & snails',
+    temp: 'hot',
+    baseFood: 'meat',
+    daysAvail: 'Friday',
+})
+let dish = Course.create({
+    category: 'secondCourse',
+    image: 'https://res.cloudinary.com/elsgerds/image/upload/v1528204566/2-3_bacalla.jpg',
+    dishName: 'Coat fish',
+    temp: 'hot',
+    baseFood: 'fish',
+    daysAvail: 'Tuesday',
+})
+let dish = Course.create({
+    category: 'secondCourse',
+    image: 'https://res.cloudinary.com/elsgerds/image/upload/v1528204566/2-5_botifarra-mongetes.jpg',
+    dishName: 'Botifarra & white beans',
+    temp: 'hot',
+    baseFood: 'meat',
+    daysAvail: 'Friday',
+})
+let dish = Course.create({
+    category: 'secondCourse',
+    image: 'https://res.cloudinary.com/elsgerds/image/upload/v1528204566/2-2_bacalla-amb-samfaina.jpg',
+    dishName: 'Coat fish & veggies',
+    temp: 'hot',
+    baseFood: 'fish',
+    daysAvail: 'Wednesday',
+})
+let dish = Course.create({
+    category: 'firstCourse',
+    image: 'https://res.cloudinary.com/elsgerds/image/upload/v1528204566/1-20_salmon-salad-and-sauce.jpg',
+    dishName: 'Salmon salad',
+    temp: 'cold',
+    baseFood: 'meat',
+    daysAvail: 'Thursday',
+})
+let dish = Course.create({
+    category: 'firstCourse',
+    image: 'https://res.cloudinary.com/elsgerds/image/upload/v1528204565/1-18_salad_plate.jpg',
+    dishName: 'Green salad',
+    temp: 'cold',
+    baseFood: 'green',
+    daysAvail: 'Wednesday',
+})
+let dish = Course.create({
+    category: 'secondCourse',
+    image: 'https://res.cloudinary.com/elsgerds/image/upload/v1528204565/2-1_bacalla-amb-escabetx-de-rovellons.jpg',
+    dishName: 'Marinated coat fish',
+    temp: 'cold',
+    baseFood: 'fish',
+    daysAvail: 'Monday',
+})
+let dish = Course.create({
+    category: 'firstCourse',
+    image: 'https://res.cloudinary.com/elsgerds/image/upload/v1528204565/1-17_risotto.png',
+    dishName: 'Risotto',
+    temp: 'hot',
+    baseFood: 'rice',
+    daysAvail: 'Wednesday',
+})
+let dish = Course.create({
+    category: 'firstCourse',
+    image: 'https://res.cloudinary.com/elsgerds/image/upload/v1528204565/1-14_kinoa-salad.jpg',
+    dishName: 'Kinoa salad',
+    temp: 'cold',
+    baseFood: 'rice',
+    daysAvail: 'Friday',
+})
+let dish = Course.create({
+    category: 'firstCourse',
+    image: 'https://res.cloudinary.com/elsgerds/image/upload/v1528204565/1-19_salad-pomegranate.jpg',
+    dishName: 'Pomegranate salad',
+    temp: 'cold',
+    baseFood: 'green',
+    daysAvail: 'Wednesday',
+})
+let dish = Course.create({
+    category: 'firstCourse',
+    image: 'https://res.cloudinary.com/elsgerds/image/upload/v1528204565/1-15_macaroni-salad.jpg',
+    dishName: 'Macaroni salad',
+    temp: 'cold',
+    baseFood: 'pasta',
+    daysAvail: 'Tuesday',
+})
+let dish = Course.create({
+    category: 'firstCourse',
+    image: 'https://res.cloudinary.com/elsgerds/image/upload/v1528204565/1-16_mango-salad.jpg',
+    dishName: 'Mango salad',
+    temp: 'cold',
+    baseFood: 'pasta',
+    daysAvail: 'Tuesday',
+})
+let dish = Course.create({
+    category: 'firstCourse',
+    image: 'https://res.cloudinary.com/elsgerds/image/upload/v1528204564/1-12_green-pasta.jpg',
+    dishName: 'Pesto pasta',
+    temp: 'cold',
+    baseFood: 'pasta',
+    daysAvail: 'Monday',
+})
+let dish = Course.create({
+    category: 'firstCourse',
+    image: 'https://res.cloudinary.com/elsgerds/image/upload/v1528204564/1-11_gazpacho.jpg',
+    dishName: 'Gazpacho',
+    temp: 'cold',
+    baseFood: 'green',
+    daysAvail: 'Monday',
+})
+let dish = Course.create({
+    category: 'firstCourse',
+    image: 'https://res.cloudinary.com/elsgerds/image/upload/v1528204564/1-13_green-spagetthi-salad.jpg',
+    dishName: 'Spaghetti salad',
+    temp: 'cold',
+    baseFood: 'green',
+    daysAvail: 'Tuesday',
+})
+let dish = Course.create({
+    category: 'firstCourse',
+    image: 'https://res.cloudinary.com/elsgerds/image/upload/v1528204564/1-9_esqueixada-bacalla.jpg',
+    dishName: 'Coat esqueixada',
+    temp: 'cold',
+    baseFood: 'fish',
+    daysAvail: 'Thursday',
+})
+let dish = Course.create({
+    category: 'firstCourse',
+    image: 'https://res.cloudinary.com/elsgerds/image/upload/v1528204564/1-6_ensaladilla-rusa.jpg',
+    dishName: 'Ensaladilla rusa',
+    temp: 'cold',
+    baseFood: 'green',
+    daysAvail: 'Friday',
+})
+let dish = Course.create({
+    category: 'firstCourse',
+    image: 'https://res.cloudinary.com/elsgerds/image/upload/v1528204564/1-10_faves-ofegades-menta.jpg',
+    dishName: 'Green broad beans',
+    temp: 'cold',
+    baseFood: 'green',
+    daysAvail: 'Monday',
+})
+let dish = Course.create({
+    category: 'firstCourse',
+    image: 'https://res.cloudinary.com/elsgerds/image/upload/v1528204564/1-5_eggplant-goatcheese.jpg',
+    dishName: 'Eggplant & goatcheese',
+    temp: 'hot',
+    baseFood: 'green',
+    daysAvail: 'Tuesday',
+})
+let dish = Course.create({
+    category: 'firstCourse',
+    image: 'https://res.cloudinary.com/elsgerds/image/upload/v1528204564/1-8_esparrecs-verds-al-forn.jpg',
+    dishName: 'Green asparagus',
+    temp: 'hot',
+    baseFood: 'green',
+    daysAvail: 'Friday',
+})
+let dish = Course.create({
+    category: 'firstCourse',
+    image: 'https://res.cloudinary.com/elsgerds/image/upload/v1528204564/1-4_carpaccio.jpg',
+    dishName: 'Carpaccio',
+    temp: 'cold',
+    baseFood: 'meat',
+    daysAvail: 'Thursday',
+})
+let dish = Course.create({
+    category: 'firstCourse',
+    image: '',
+    dishName: '',
+    temp: 'cold',
+    baseFood: 'pasta',
+    daysAvail: '',
+})
+let dish = Course.create({
+    category: 'firstCourse',
+    image: '',
+    dishName: '',
+    temp: 'cold',
+    baseFood: 'pasta',
+    daysAvail: '',
+})
+let dish = Course.create({
+    category: 'firstCourse',
+    image: '',
+    dishName: '',
+    temp: 'cold',
+    baseFood: 'pasta',
+    daysAvail: '',
+})
+let dish = Course.create({
+    category: 'firstCourse',
+    image: '',
+    dishName: '',
+    temp: 'cold',
+    baseFood: 'pasta',
+    daysAvail: '',
+})
+let dish = Course.create({
+    category: 'firstCourse',
+    image: '',
+    dishName: '',
+    temp: 'cold',
+    baseFood: 'pasta',
+    daysAvail: '',
+})
+let dish = Course.create({
+    category: 'firstCourse',
+    image: '',
+    dishName: '',
+    temp: 'cold',
+    baseFood: 'pasta',
+    daysAvail: '',
+})
+let dish = Course.create({
+    category: 'firstCourse',
+    image: '',
+    dishName: '',
+    temp: 'cold',
+    baseFood: 'pasta',
+    daysAvail: '',
+})
+let dish = Course.create({
+    category: 'firstCourse',
+    image: '',
+    dishName: '',
+    temp: 'hot',
+    baseFood: 'pasta',
+    daysAvail: '',
+})
+
+
 
 
 // https://cloudinary.com/console/media_library/folders/images/
