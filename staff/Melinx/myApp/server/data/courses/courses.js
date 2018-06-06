cdcec135be5cc185dd875763320451f40404cabd
Melinx <indraroura@gmail.com>
2018-06-06 11:29:45 +0200

    d

--- staff/Melinx/myApp/server/data/courses/courses.js
+++ staff/Melinx/myApp/server/data/courses/courses.js
@@ -10,12 +10,29 @@ let dish = Course.create({
 
 let dish = Course.create({
     category: 'secondCourse',
-    image: 'https://res.cloudinary.com/elsgerds/image/upload/v1528204568/2-20_tuna-tataki.jpg',
-    dishName: 'Tuna Tataki',
-    temp: 'cold',
+    image: 'https://res.cloudinary.com/elsgerds/image/upload/v1528204567/2-19_trinxat.jpg',
+    dishName: 'Trinxat',
+    temp: 'hot',
+    baseFood: 'green',
+    daysAvail: 'Wednesday',
+})
+
+let dish = Course.create({
+    category: 'secondCourse',
+    image: 'https://res.cloudinary.com/elsgerds/image/upload/v1528204567/2-12_paella-mar-muntanya.jpg',
+    dishName: 'Paella',
+    temp: 'hot',
+    baseFood: 'rice',
+    daysAvail: 'Thurssday',
+})
+let dish = Course.create({
+    category: 'secondCourse',
+    image: 'https://res.cloudinary.com/elsgerds/image/upload/v1528204567/2-17_salmon-teryaki.jpg',
+    dishName: 'Salmon Teryaki',
+    temp: 'hot',
     baseFood: 'fish',
-    daysAvail: 'Monday',
-}),
+    daysAvail: 'Wednesday',
+})
 
 
 // https://cloudinary.com/console/media_library/folders/images/
